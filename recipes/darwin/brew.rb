edb358704c8a1cd8c12f5428d22b3f71f9edbe64
argonism <44169535+argonism@users.noreply.github.com>
2025-08-13 21:50:12 +0900

    Add brew recipe

---
+++ recipes/darwin/brew.rb
@@ -0,0 +1,10 @@
+execute "Install homebrew" do
+  command <<-EOH
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  EOH
+  not_if "which brew"
+end
+
+execute "Install homebrew packages" do
+  command "brew bundle --file=#{File.expand_path('../files/Brewfile', __FILE__)}"
+end
