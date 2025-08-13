8861d767562b70a7cadcf350b04ee98ffec30399
argonism <44169535+argonism@users.noreply.github.com>
2025-08-14 00:10:21 +0900

    Remove process for spin

--- install.sh
+++ install.sh
@@ -4,12 +4,6 @@ set -ex
 
 bin/setup
 
-# Use a special setup on spin
-if [[ -n "$SPIN" ]]; then
-  sudo -E bin/mitamae local $@ recipes/spin/default.rb
-  exit
-fi
-
 # Homebrew does not allow sudo.
 case "$(uname)" in
   "Darwin")  bin/mitamae local $@ recipes/default.rb ;;
