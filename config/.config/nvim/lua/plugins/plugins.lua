74b2fc9e973c28c75e5487d1f951664cab120e12
argonism <44169535+argonism@users.noreply.github.com>
2025-08-22 15:09:42 +0900

    Add notify plugin

--- config/.config/nvim/lua/plugins/plugins.lua
+++ config/.config/nvim/lua/plugins/plugins.lua
@@ -78,5 +78,22 @@ return {
   { 'github/copilot.vim' },
   {
     'HiPhish/rainbow-delimiters.nvim',
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+        },
+      })
+    end
   }
 }
