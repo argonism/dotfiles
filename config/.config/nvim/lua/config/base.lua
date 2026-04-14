9ea190ee39db430435435de1229d7cca58b49381
argonism <44169535+argonism@users.noreply.github.com>
2026-04-14 23:50:10 +0900

    style(nvim): normalize keymap option table spacing
    Add consistent spaces inside `{ ... }` option tables and break the
    <C-c> keymap options across lines to match the surrounding style.

--- config/.config/nvim/lua/config/base.lua
+++ config/.config/nvim/lua/config/base.lua
@@ -15,14 +15,15 @@ vim.opt.number = true
 vim.g.mapleader = " "
 vim.api.nvim_set_keymap("i", "jj", "<esc>", { noremap = true })
 vim.api.nvim_set_keymap("i", "kk", "<esc>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-h>", ":BufferLineCyclePrev<CR>", {noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-l>", ":BufferLineCycleNext<CR>", {noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-h>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-l>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-c>", "", {
-  noremap = true, silent = true,
+  noremap = true,
+  silent = true,
   callback = function() require("mini.bufremove").delete(0, false) end,
 })
-vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", {noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>b", ":BufferLinePick<CR>", {noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>b", ":BufferLinePick<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", ">", ">gv", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<", "<gv", { noremap = true, silent = true })
 
